6855a458d5ef504306f4e3b336c48a1a14052a3d
Utpanna Pradhan <Utpanna828@gmail.com>
2025-03-26 23:53:20 +0530

    first commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -14,7 +14,7 @@ export default function Experience() {
             setIsVisible(true);
             observer.unobserve(entry.target);
           }
-        });
+        }); 
       },
       { threshold: 0.1 }
     );
