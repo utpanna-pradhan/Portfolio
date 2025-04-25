097916ebcd70d4c97f3f8f9a147843bdad2d5770
Utpanna Pradhan <Utpanna828@gmail.com>
2025-04-25 20:00:43 +0530

    work changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,12 @@
 import p1 from '../assets/p1.png';
+import p2 from '../assets/p2.png';
+import p3 from '../assets/p3.png';
+import p4 from '../assets/p4.png';
+import p5 from '../assets/p5.png';
+import p6 from '../assets/p6.png';
+import p7 from '../assets/p7.png';
+import p8 from '../assets/p8.png';
+
 
 import '../stylesheet/Work.css';
 export default function Work() {
@@ -14,38 +22,55 @@ export default function Work() {
     },
     {
       id: 2,
-      img:p1,
-      title: "Heartitout Blog page",
-      description: "Heart it out's blog page",
+      img:p2,
+      title: "Heartitout Academy page",
+      description: "It shows all upcomming events",
       tags: ["HTML","CSS", "JavaScript","Bootstrap5","Jquery"]
     },
     {
-      id: 2,
-      img:p1,
-      title: "Portfolio Template",
-      description: "A customizable portfolio template for creatives.",
-      tags: ["HTML/CSS, ", "JavaScript"]
+      id: 3,
+      img:p3,
+      title: "Reco Engine",
+      description: "It helps to find perfect Therapist based on your input",
+      tags: ["HTML, CSS , JavaScript , Jquery , Bootstrap5 , API "]
     },
     {
-      id: 2,
-      img:p1,
+      id: 4,
+      img:p4,
       title: "Portfolio Template",
       description: "A customizable portfolio template for creatives.",
       tags: ["HTML/CSS", "JavaScript"]
     },
     {
-      id: 2,
-      img:p1,
+      id: 5,
+      img:p5,
       title: "Portfolio Template",
       description: "A customizable portfolio template for creatives.",
       tags: ["HTML/CSS", "JavaScript"]
     },
     {
-      id: 2,
-      img:p1,
+      id: 6,
+      img:p6,
       title: "Portfolio Template",
       description: "A customizable portfolio template for creatives.",
-      tags: ["HTML/CSS", "JavaScript"]
+      tags: ["HTML/CSS", "JavaScript"],
+      src:"https://github-profile-analyzer-navy.vercel.app/"
+    },
+    {
+      id: 7,
+      img:p7,
+      title: "Portfolio Template",
+      description: "A customizable portfolio template for creatives.",
+      tags: ["HTML/CSS", "JavaScript"],
+      src:"https://utpanna-pradhan.github.io/Chat_app/"
+    },
+    {
+      id: 8,
+      img:p8,
+      title: "Portfolio Template",
+      description: "A customizable portfolio template for creatives.",
+      tags: ["HTML/CSS", "JavaScript"],
+      src:"https://heartitout.in/jiya-therapist-finder-demo/"
     }
   ];
 
@@ -77,7 +102,9 @@ export default function Work() {
       </div> */}
 
 <div className="gallery-grid">
+ 
         {projects.map((project) => (
+           <a href={project.src} target='_blank' >
           <div className="gallery-item" key={project.id}>
             <img src={project.img} alt={project.title} />
            
@@ -85,13 +112,15 @@ export default function Work() {
               <h3>{project.title}</h3>
               <p>{project.description}</p>
               <p>{project.tags}</p>
-              <a href={project.src} target='_blank' className='text-white'>Link</a>
-             
+              {/* <a href= target='_blank' className='text-white'>Link</a> */}
+            
             </div>
           </div>
+          </a>
         ))}
+        
       </div>
-     
+    
      
     </section>
   );
